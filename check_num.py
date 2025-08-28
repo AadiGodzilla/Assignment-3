18d62b9ee11eb6e88e1afb7bddd766f0013d3cce
AadiGodzilla <bhandari.anjan@gmail.com>
2025-08-28 12:42:15 +0545

    check_num.py: wrote comments greet.py: wrote comments

--- check_num.py
+++ check_num.py
@@ -1,12 +1,18 @@
-number = int(input("Enter a number: "))     #Prompt the user to enter a number
+#Prompt the user to enter a number
+number = int(input("Enter a number: "))     
 
-if number > 0 and number % 2 == 0:          # Check if the number is positive and even
+# Check if the number is positive and even
+if number > 0 and number % 2 == 0:          
     print("Number is a positive even number") 
-elif number > 0 and number % 2 != 0:        # Check if the number is positive and odd
+# Check if the number is positive and odd
+elif number > 0 and number % 2 != 0:        
     print("Number is a positive odd number")
-elif number < 0 and number % 2 == 0:        # Check if the number is negative and even
+# Check if the number is negative and even
+elif number < 0 and number % 2 == 0:        
     print("Number is a negative even number")
-elif number < 0 and number % 2 != 0:        # Check if the number is negative and odd
+# Check if the number is negative and odd
+elif number < 0 and number % 2 != 0:        
     print("Number is a negative odd number")
-else:                                       # Otherwise, the number is zero
+# Otherwise, the number is zero
+else:                                       
     print("Number is zero")
--- greet.py
+++ greet.py
@@ -1,15 +1,20 @@
-greeting = input("Enter a message: ")   # Get greeting string from user
-lower_greeting = greeting.lower()   # Convert the greeting string to lowercase 
+# Get greeting string from user
+greeting = input("Enter a message: ")   
+# Convert the greeting string to lowercase
+lower_greeting = greeting.lower()        
 
 # Separate string into a list of words with " " as separator
 list = lower_greeting.split(" ")    
 
-if list[0] == 'hello':  # if the first word in the greeting string start with 'hello', set output to 0
+# if the first word in the greeting string start with 'hello', set output to 0
+if list[0] == 'hello':          
     output = 0
-elif lower_greeting[0] == 'h':  # else if the first letter in the greeting string starts with letter 'h', set output to 20
+# else if the first letter in the greeting string starts with letter 'h', set output to 20
+elif lower_greeting[0] == 'h':  
     output = 20
-else:
-    output = 100       # else, set output to 100
+# else, set output to 100
+else:                           
+    output = 100      
 
 # Display output
 print("Output: $", output)
