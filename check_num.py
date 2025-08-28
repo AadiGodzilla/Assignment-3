cc4e6b85aadc0feda9497444270aed0b1ae5429e
AadiGodzilla <bhandari.anjan@gmail.com>
2025-08-28 12:18:49 +0545

    wrote comments

--- check_num.py
+++ check_num.py
@@ -1,10 +1,10 @@
-number = int(input("Enter a number: "))
+number = int(input("Enter a number: "))     #Prompt the user to enter a number
 
-if number > 0 and number % 2 == 0:
-    print("Number is a positive even number")
-elif number > 0 and number % 2 != 0:
+if number > 0 and number % 2 == 0:          # Check if the number is positive and even
+    print("Number is a positive even number") 
+elif number > 0 and number % 2 != 0:        # Check if the number is positive and odd
     print("Number is a positive odd number")
-elif number < 0 and number % 2 == 0:
+elif number < 0 and number % 2 == 0:        # Check if the number is negative and even
     print("Number is a negative even number")
-else:
+else:                                       # Else, display that number is negative and odd
     print("Number is a negative odd number")
--- greet.py
+++ greet.py
@@ -1,13 +1,15 @@
-greeting = input("Enter a message: ")
-lower_greeting = greeting.lower()
+greeting = input("Enter a message: ")   # Get greeting string from user
+lower_greeting = greeting.lower()   # Convert the greeting string to lowercase 
 
-list = lower_greeting.split(" ")
+# Separate string into a list of words with " " as separator
+list = lower_greeting.split(" ")    
 
-if list[0] == 'hello':
+if list[0] == 'hello':  # if the first word in the greeting string start with 'hello', set output to 0
     output = 0
-elif lower_greeting[0] == 'h':
+elif lower_greeting[0] == 'h':  # else if the first letter in the greeting string starts with letter 'h', set output to 20
     output = 20
 else:
-    output = 100
+    output = 100       # else, set output to 100
 
+# Display output
 print("Output: $", output)
